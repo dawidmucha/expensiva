6f1a0a776cbc3b7d142bfe4f290994f9e2049f96
Dawid Mucha <dawidmucha98@gmail.com>
2020-01-02 20:38:48 +0100

    Updates

---
+++ precache-manifest.43fbb2f344ff804e91e0f6177f69c7b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efb55597874b2f23e8ed0b9702b2dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef77ad0d630bcebf881c",
+    "url": "/static/css/main.22bd1421.chunk.css"
+  },
+  {
+    "revision": "921c4b2487551e95d3d8",
+    "url": "/static/js/2.7d79df76.chunk.js"
+  },
+  {
+    "revision": "ef77ad0d630bcebf881c",
+    "url": "/static/js/main.88630ef5.chunk.js"
+  },
+  {
+    "revision": "db3444cd469fcaed4ea1",
+    "url": "/static/js/runtime-main.d06629ac.js"
+  },
+  {
+    "revision": "df4fd8afd67b49dcdace5e39bcefeb1b",
+    "url": "/static/media/bg1.df4fd8af.svg"
+  }
+]);
