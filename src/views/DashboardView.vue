c1ae1e396f9c51392f9e2dd6a2583681c94b8abe
Dawid Mucha <dawidmucha98@gmail.com>
2025-07-04 18:04:08 +0200

    receipts fetched on load

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,14 +2,17 @@
 import SideBar from '@/components/SideBar.vue'
 import axios from 'axios'
 
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuth0 } from '@auth0/auth0-vue'
 
 const { user } = useAuth0()
 const router = useRouter()
 
+const receipts = ref([])
+
 const addReceipt = () => {
+  console.log('addsub?', user.value.sub)
   axios
     .post('http://localhost:8081/receipt', {
       userId: user.value.sub,
@@ -22,8 +25,25 @@ const addReceipt = () => {
     })
 }
 
+const getReceipts = () => {
+  console.log('getsub?', user.value.sub)
+  axios
+    .put('http://localhost:8081/receipt', {
+      userId: user.value.sub,
+    })
+    .then((res) => {
+      console.log('ee', res.data.results)
+      receipts.value = res.data.results
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
 onMounted(async () => {
   if (user == undefined) return router.push('/')
+
+  getReceipts()
 })
 </script>
 
@@ -31,7 +51,10 @@ onMounted(async () => {
   <SideBar />
   <div id="dashboardView">
     <div>dashboard view</div>
+    <h6>receipts:</h6>
+    <div>{{ receipts }}</div>
     <button @click="addReceipt">add receipt</button>
+    <button @click="getReceipts">get receipt</button>
   </div>
 </template>
 
